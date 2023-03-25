2cb2c0c3cff12e7c87bbe1f30f9c317825d036a8
panamadalina <madalina.pana1997@gmail.com>
2023-03-25 15:54:57 +0200

    no message

---
+++ src/server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const favicon = require('serve-favicon');
+const path = require('path');
+const utils = require('./utils');
+const { textToSpeech } = require('./azure-cognitiveservices-speech');
+
+// creates a temp file on server, the streams to client
+/* eslint-disable no-unused-vars */
+
+// fn to create express server
+const create = async () => {
+
+    // server
+    const app = express();
+    app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
+    
+    // Log request
+    app.use(utils.appLogger);
+
+    // root route - serve static file
+    app.get('/api/hello', (req, res) => {
+        res.json({hello: 'goodbye'});
+        res.end();
+    });
+
+    // root route - serve static file
+    app.get('/', (req, res) => {
+        return res.sendFile(path.join(__dirname, '../public/client.html'));
+
+    });
+
+    app.get('/text-to-speech', async (req, res, next) => {
+    
+        const { key, region, phrase, file } = req.query;
+        
+        if (!key || !region || !phrase) res.status(404).send('Invalid query string');
+        
+        let fileName = null;
+        
+        // stream from file or memory
+        if (file && file === true) {
+            fileName = `./temp/stream-from-file-${timeStamp()}.mp3`;
+        }
+        
+        const audioStream = await textToSpeech(key, region, phrase, fileName);
+        res.set({
+            'Content-Type': 'audio/mpeg',
+            'Transfer-Encoding': 'chunked'
+        });
+        audioStream.pipe(res);
+    });
+    
+
+    // Catch errors
+    app.use(utils.logErrors);
+    app.use(utils.clientError404Handler);
+    app.use(utils.clientError500Handler);
+    app.use(utils.errorHandler);
+
+    return app;
+};
+
+module.exports = {
+    create
+};
